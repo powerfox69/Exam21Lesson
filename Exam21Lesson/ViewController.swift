3cbe01f4b33465378fa14d61d71f9b1e894468cf
Gleb Mosko <gleb.yandex.ru>
2025-09-02 23:12:41 +0300

    первый этап

--- Exam21Lesson/ViewController.swift
+++ Exam21Lesson/ViewController.swift
@@ -1,19 +1,57 @@
-//
-//  ViewController.swift
-//  Exam21Lesson
-//
-//  Created by Gleb on 9/2/25.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    private var textLabel = UILabel()
+    private let stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViewController()
+        
     }
 
 
 }
 
+//MARK: - Setup view
+
+private extension ViewController {
+    
+    func setupViewController() {
+        
+        view.backgroundColor = .darkGray
+        setupStackView()
+        setupLabel()
+        view.addSubview(stackView)
+        setupLayout()
+    }
+    
+    func setupStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .equalCentering
+        stackView.alignment = .center
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.addArrangedSubview(textLabel)
+        
+    }
+    
+    func setupLabel() {
+        textLabel.text = "крым"
+        textLabel.font = .systemFont(ofSize: 50, weight: .bold)
+        textLabel.textColor = .systemGray6
+        textLabel.textAlignment = .center
+    }
+    
+    func setupLayout() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 500),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+            
+        }
+}
